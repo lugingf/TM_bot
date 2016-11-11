0938caa4720ac5e2f034f01a9dedcc8fbf7dc603
lugingf <lugingf@gmail.com>
2016-11-12 00:03:24 +0300

    Исправлены некоторые ошибки

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import requests
 import time
 import re
 
-from config import interval, phrases_list, hello_list, who_is_bot, yes_answers, i_am_list
+from config import interval, phrases_list, hello_list, who_is_bot, yes_answers, i_am_list, what_are_you_talking
 from weather import forecast_weather_req
 
 bot = telebot.TeleBot(tok.tok)
@@ -48,33 +48,36 @@ def i_am_here(message):
 
 @bot.message_handler(regexp=r'^[мМ]айор,?')
 def added_func_parsing(message):
-	pog_trig = re.search(r'[пП][оО][гГ][оО][дД]', message.text).group().lower()
-	if pog_trig == 'погод':
-		arg_words = message.text[6:].split()
-		try:
-			city = re.search(r'\sво?\s(?P<city>\w+)\??', message.text).group('city')
-			if 'завтра' in arg_words:
-				when_day = 'tomorrow'
-			elif 'послезавтра' in arg_words:
-				when_day = 'after_tomorrow'
-			else:
-				when_day = None
-			if 'утро' in arg_words or 'утра' in arg_words or 'утром' in arg_words:
-				when_time = 'morn'
-			elif 'день' in arg_words or 'днем' in arg_words:
-				when_time = 'day'
-			elif 'вечер' in arg_words or 'вечера' in arg_words:
-				when_time = 'evn'
-			elif 'ночь' in arg_words or 'ночью' in arg_words or 'ноч' in arg_words:
-				when_time = 'night'
-			else:
-				when_time = None
-			#res = ' '.join((when_time or ' ', when_day or ' ', city or ' '))
-			res = flatten(forecast_weather_req(city, when_day, when_time))
-			go_text = 'В {} будет примерно {}\xB0С, {}. Инфа от станции "{}"'.format(city, res[1], res[2], res[0])
-			bot.send_message(message.chat.id, go_text)
-		except AttributeError:
-			bot.send_message(message.chat.id, 'Ты не сказал где')
+	try:
+		pog_trig = re.search(r'[пП][оО][гГ][оО][дД]', message.text).group().lower()
+		if pog_trig == 'погод':
+			arg_words = message.text[6:].split()
+			try:
+				city = re.search(r'\sво?\s(?P<city>\w+)\??', message.text).group('city')
+				if 'завтра' in arg_words:
+					when_day = 'tomorrow'
+				elif 'послезавтра' in arg_words:
+					when_day = 'after_tomorrow'
+				else:
+					when_day = None
+				if 'утро' in arg_words or 'утра' in arg_words or 'утром' in arg_words:
+					when_time = 'morn'
+				elif 'день' in arg_words or 'днем' in arg_words:
+					when_time = 'day'
+				elif 'вечер' in arg_words or 'вечера' in arg_words:
+					when_time = 'evn'
+				elif 'ночь' in arg_words or 'ночью' in arg_words or 'ноч' in arg_words:
+					when_time = 'night'
+				else:
+					when_time = None
+				#res = ' '.join((when_time or ' ', when_day or ' ', city or ' '))
+				res = flatten(forecast_weather_req(city, when_day, when_time))
+				go_text = 'В {} будет примерно {}\xB0С, {}. Инфа от станции "{}"'.format(city, res[1], res[2], res[0])
+				bot.send_message(message.chat.id, go_text)
+			except AttributeError:
+				bot.send_message(message.chat.id, 'Ты не сказал где')
+	except AttributeError:
+		bot.send_message(message.chat.id, random.choice(what_are_you_talking))
 
 
 @bot.message_handler(regexp=r'(\sбот\s)|(^[бБ]от\?$)|(\s[бБ]от\?)|(^[бБ]от$)')
