f323cc9966117819ec16786871ba4da335dd9a39
lugingf <lugingf@gmail.com>
2016-11-11 23:46:55 +0300

    чистка

--- weather.py
+++ weather.py
@@ -1,36 +1,5 @@
 # -*- coding: utf-8 -*-
 
-'''
- payload = {'key1': 'value1', 'key2': 'value2'}
- r = requests.get('http://httpbin.org/get', params=payload)
- r.json()
- print(r.url)
-http://httpbin.org/get?key1=value1&key2=value2&key2=value3
-'Конвертировать время POSIX в datetime'
-print datetime.fromtimestamp(time.time())
-#> 2010-08-14 02:58:15.057613
-d = datetime.today()
-print d
-#> 2010-08-14 02:58:15.057538
-'Сдвинуть время'
-print d + timedelta(days=1, hours=1)
-#> 2010-08-15 03:58:15.057538
-'Получить день недели, 0 - Пн, 6 - Вс'
-print d.weekday()
-#> 5
-скажи погоду дата/время
-какая погода дата/время
-что там по поводу погоды дата/время
-что там по погоде дата/время
-погода дата/время
-по датам: сегодня/завтра/послезавтра
-а время: ночь/утро/день/вечер
-result_examples:
-current_weather.json()
-{'coord': {'lon': 37.62, 'lat': 55.75}, 'base': 'stations', 'sys': {'type': 1, 'country': 'RU', 'message': 0.0051, 'sunset': 1478612009, 'sunrise': 1478580743, 'id': 7323}, 'wind': {'deg': 240, 'speed': 4}, 'main': {'pressure': 1012, 'temp_max': 2, 'temp_min': 0, 'humidity': 94, 'temp': 1.01}, 'visibility': 10000, 'weather': [{'main': 'Clouds', 'icon': '04n', 'description': 'пасмурно', 'id': 804}], 'id': 524901, 'dt': 1478622600, 'cod': 200, 'name': 'Moscow', 'clouds': {'all': 90}}
-forecast.json()['list'][0]:
-{'dt': 1478628000, 'snow': {'3h': 0.027}, 'rain': {}, 'sys': {'pod': 'n'}, 'wind': {'deg': 288.501, 'speed': 2.57}, 'main': {'pressure': 1007.74, 'temp_kf': 2.39, 'temp_min': -2.84, 'humidity': 94, 'temp': -0.45, 'grnd_level': 1007.74, 'temp_max': -0.45, 'sea_level': 1028.51}, 'dt_txt': '2016-11-08 18:00:00', 'clouds': {'all': 80}, 'weather': [{'main': 'Clear', 'icon': '01n', 'description': 'ясно', 'id': 800}]}
-'''
 
 import requests
 import time
@@ -45,7 +14,6 @@ def forecast_weather_format(forecast_data_list, when_day=None, when_time=None):
 	curr_time = datetime.today()
 	tomorrow_day = (curr_time + timedelta(days=1)).day
 	after_tomorrow_day = (curr_time + timedelta(days=2)).day
-	res = []
 	for i in range(len(forecast_data_list) - 1):
 		weather_dt = datetime.fromtimestamp(forecast_data_list[i]['dt'])
 		if when_day == "tomorrow" and when_time == "morn":  # TODO: 1) изменить структуру условий, объединив when_day и weather_dt; 2) слишком много одинакового кода
@@ -135,9 +103,5 @@ if __name__ == '__main__':
 	# print(forecast_weather_req('Владивосток', 'tomorrow', 'evn'))
 	# print(forecast_weather_req('Rostov'))
 
-	'''
-	print('Погода-хуегода в ', loc_now, temp_now, '\xB0С')
-	print(current_weather.json())
-	print(forecast.json()['list'][0])
-	for i in range(len(forecast.json()['list'])):
-		print('Температура на', datetime.fromtimestamp(forecast_data[i]['dt']), 'где-то', int(forecast_data[i]['main']['temp']), '\xB0С')  # \xB0С - символ градуса'''
+
+ # \xB0С - символ градуса
